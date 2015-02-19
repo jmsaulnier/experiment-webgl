7987814357c9ee8c725dfb435571d4ff6f1339db
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2015-02-18 16:54:31 -0800

    Added fx / DefferedPointLIght - Started Terrain

--- app/src/main.js
+++ app/src/main.js
@@ -7,6 +7,8 @@ var glu = require('pex-glu');
 var materials = require('pex-materials');
 var color = require('pex-color');
 var gen = require('pex-gen');
+var helpers = require('pex-helpers');
+var rnd = require('pex-random');
 
 /**
  * main module.
@@ -15,33 +17,54 @@ var gen = require('pex-gen');
 
 domready(function () {
 
+  var Plane = gen.Plane;
 
-  var Cube = gen.Cube;
-  var Mesh = glu.Mesh;
-  var ShowNormals = materials.ShowNormals;
-  var PerspectiveCamera = glu.PerspectiveCamera;
-  var Arcball = glu.Arcball;
-  var Color = color.Color;
-  var Platform = sys.Platform;
 
   sys.Window.create({
     settings: {
       width: 1280,
       height: 720,
       type: '3d',
-      fullscreen: Platform.isBrowser ? true : false
+      fullscreen: sys.Platform.isBrowser ? true : false
     },
     init: function() {
-      var cube = new Cube();
-      this.mesh = new Mesh(cube, new ShowNormals());
 
-      this.camera = new PerspectiveCamera(60, this.width / this.height);
-      this.arcball = new Arcball(this, this.camera);
+      this.plane = new Plane(5,5,10,10);
+
+      this.terrain = new glu.Mesh(this.plane, new materials.SolidColor({ color: color.Color.Grey }));
+
+      this.camera = new glu.PerspectiveCamera(60, this.width / this.height);
+      //this.arcball = new glu.Arcball(this, this.camera, 2);
+
+      this.helperMeshes = [];
+      this.helperMeshes.push(new helpers.VertexHelper(this.plane));
+      this.helperMeshes.push(new helpers.FaceHelper(this.plane));
+
     },
     draw: function() {
-      glu.clearColorAndDepth(Color.Red);
+
+      glu.viewport(0, 0, this.width, this.height);
+      glu.clearColorAndDepth(color.Color.White);
       glu.enableDepthReadAndWrite(true);
-      this.mesh.draw(this.camera);
+
+      var plane = this.plane;
+
+      this.terrain.geometry.vertices.forEach(function(v, vi) {
+        var n = plane.vertices[vi].dup().normalize()
+        var f = 0.01 * rnd.noise3(n.x, n.y, n.z + sys.Time.seconds);
+        v.setVec3(plane.vertices[vi]);
+        v.add(n.scale(f));
+      });
+      
+      this.terrain.geometry.vertices.dirty = true;
+      this.terrain.geometry.computeNormals();
+
+      this.terrain.draw(this.camera);
+
+      this.helperMeshes.forEach(function(helper) {
+        helper.draw(this.camera);
+      }.bind(this));
+
     }
   });
 
